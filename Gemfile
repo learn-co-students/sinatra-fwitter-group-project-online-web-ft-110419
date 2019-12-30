6bc6457f0d5771c5cefe643b19f815832840bf96
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2019-12-30 13:09:14 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -12,9 +12,9 @@ gem 'pry'
 gem 'bcrypt'
 gem "tux"
 
-group :test do
+# group :test do
   gem 'rspec'
   gem 'capybara'
   gem 'rack-test'
   gem 'database_cleaner'
-end
+# end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
 
 
+  get '/signup' do
+    # if logged_in?
+    #     redirect '/tweets'
+    # else 
+         erb :'users/signup'
+    # end 
+  end 
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect '/signup'
+    else 
+        @user = User.new(
+            :username => params["username"],
+            :email => params["email"],
+            :password_digest => params["password"])
+        session[:user_id] = @user.id
+        @user.save
+        # binding.pry
+        redirect '/tweets'
+    end 
+  end 
+
 end
